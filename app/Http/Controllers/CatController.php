589600c658038ea6e357547d8d1a9974352c7e24
Milton-Zua <miltonzua972@gmail.com>
2025-06-12 10:58:11 +0100

    after created the catController

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -1,10 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CatController extends Controller
 {
-    //
+    public function show()
+    {
+        try {
+            $response = Http::withHeaders([
+                'x-api-key' => config('services.catapi.key')
+            ])->get(config('services.catapi.endpoint'));
+
+            if ($response->successful()) {
+                return response()->json([
+                    'status' => 'success',
+                    'data' => $response->json(),
+                ]);
+            }
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Erro na chamada da API.',
+                'code' => $response->status(),
+            ], $response->status());
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'exception',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
